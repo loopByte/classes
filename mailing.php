129eba76144e9c1e38ba30fe937532d60709b9ae
Vasile Milea <milea.vasile959@gmail.com>
2014-04-02 22:31:30 +0300

    Major changes
    -Added error reporting for debugging
    -Fixed __construct method
    -Fixed single method checking for properties
    -Fixed return value after email was sended
    -Added subscribe method

--- mailing.php
+++ mailing.php
@@ -1,27 +1,60 @@
 <?php
 
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    
     class Mailing{
         public $to;
         public $subject;
         public $message;
         public $headers = array('From: admin@loopbyte.tk');
         
-        public function __construct{}
+        public function __construct(){}
         
         public function single(){
-            if(!empty($this->to, $this->subject, $this->message, $this->headers)){
+            if( !empty($this->to) ||
+                !empty($this->subject) ||
+                !empty($this->message) ||
+                !empty($this->headers)){
                 if(is_array($this->to)){
-                    exit("You have called the wrong method to send the e-mail.")
+                    exit("You have called the wrong method to send the e-mail.");
                 }else{
                     $headers = implode(',', $this->headers);
                     $send = mail($this->to, $this->subject, $this->message, $headers);
-                    ($send) ? return true : return false;
+                    if($send){return true;}else{return false;}
                 }
             }else{
                 exit("You have to fill in the required fields.");
             }
         }
         
-        public function subscribe(){}
+        public function subscribe(){
+            if( !empty($this->to) ||
+                !empty($this->subject) ||
+                !empty($this->message) ||
+                !empty($this->headers)){
+                    if(is_array($this->to)){
+                        $headers = implode(',', $this->headers);
+                        foreach($this->to as $to){
+                            $send = mail($to, $this->subject, $this->message, $headers);
+                        }
+                        if($send){return true;}else{return false;}
+                    }else{
+                        exit('To property is not correctly set.');
+                    }
+            }else{
+                exit('Properties are not correctly set.');
+            }
+        }
     }
+    
+    $mail = new Mailing();
+    
+    $mail->to = array('milea.vasile959@gmail.com', 'milea.vasile959@gmail.com');
+    $mail->subject = 'This is a subject';
+    $mail->message = 'This is a message';
+    $mail->headers = array('From: admin@loopbyte.tk');
+    
+    echo $mail->subscribe();
+    
 ?>
