7419818b6656d47ce77952ee77249f587913bfb5
loopByte <milea.vasile959@gmail.com>
2014-04-02 11:53:57 +0300

    Single email method
    -Added sngle email send method

--- mailing.php
+++ mailing.php
@@ -1,1 +1,27 @@
 <?php
+
+    class Mailing{
+        public $to;
+        public $subject;
+        public $message;
+        public $headers = array('From: admin@loopbyte.tk');
+        
+        public function __construct{}
+        
+        public function single(){
+            if(!empty($this->to, $this->subject, $this->message, $this->headers)){
+                if(is_array($this->to)){
+                    exit("You have called the wrong method to send the e-mail.")
+                }else{
+                    $headers = implode(',', $this->headers);
+                    $send = mail($this->to, $this->subject, $this->message, $headers);
+                    ($send) ? return true : return false;
+                }
+            }else{
+                exit("You have to fill in the required fields.");
+            }
+        }
+        
+        public function subscribe(){}
+    }
+?>
